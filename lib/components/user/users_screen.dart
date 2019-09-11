ef5bc6755e642ae44b46a9c6c030759a32aac2c5
Carlos Lira <carlos.lira@tempest.com.br>
2019-09-11 10:17:49 -0300

    There is a better way of doing things on init, fixing that

--- lib/components/user/users_screen.dart
+++ lib/components/user/users_screen.dart
@@ -1,29 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 import 'package:redux/redux.dart';
 
-import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_with_redux/routes.dart';
 
 import 'package:flutter_with_redux/models/app_state.dart';
+
+import 'package:flutter_with_redux/models/user/user.dart';
 import 'package:flutter_with_redux/models/user/user_state.dart';
 
 import 'package:flutter_with_redux/actions/user_actions.dart';
 
-import 'package:flutter_with_redux/components/user/users_list.dart';
-
 class UsersScreen extends StatelessWidget {
+  void handleInitialBuild(UsersScreenProps props) {
+    props.getUsers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, UsersScreenProps>(
       converter: (store) => mapStateToProps(store),
+      onInitialBuild: (props) => this.handleInitialBuild(props),
       builder: (context, props) {
+        List<User> data = props.listResponse.data;
+        bool loading = props.listResponse.loading;
+
+        Widget body;
+        if (loading) {
+          body = Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          body = ListView.separated(
+            padding: const EdgeInsets.all(16.0),
+            itemCount: data.length,
+            separatorBuilder: (context, index) => Divider(),
+            itemBuilder: (context, i) {
+              User user = data[i];
+
+              return ListTile(
+                title: Text(
+                  user.name,
+                ),
+                onTap: () {
+                  props.getUserDetails(user.id);
+                  Navigator.pushNamed(context, AppRoutes.userDetails);
+                },
+              );
+            },
+          );
+        }
+
         return Scaffold(
           appBar: AppBar(
             title: Text('Users list'),
           ),
-          body: UsersList(
-            onInit: props.getUsers,
-          ),
+          body: body,
         );
       },
     );
@@ -32,11 +65,13 @@ class UsersScreen extends StatelessWidget {
 
 class UsersScreenProps {
   final Function getUsers;
+  final Function getUserDetails;
   final ListUsersState listResponse;
 
   UsersScreenProps({
     this.getUsers,
     this.listResponse,
+    this.getUserDetails,
   });
 }
 
@@ -44,5 +79,6 @@ UsersScreenProps mapStateToProps(Store<AppState> store) {
   return UsersScreenProps(
     listResponse: store.state.user.list,
     getUsers: () => store.dispatch(getUsers()),
+    getUserDetails: (int id) => store.dispatch(getUserDetails(id)),
   );
 }
